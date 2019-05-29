155466e70a2862220eeb00e29f2b64fa5a4d9e7a
Wayne0807 <b06703021@ntu.edu.tw>
2019-05-29 20:26:35 +0800

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,16 +1,1 @@
-<<<<<<< HEAD
-start here
-
-please
-
-print("HI")
-
-print(2)
-
-class shitGitHub():
-	print("really damn")
-	
 print(1)
-=======
-"這個很難用"
->>>>>>> d45ecdc5fe2236ffbd68978bb8843d47820741f0
